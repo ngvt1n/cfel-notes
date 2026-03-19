11ca04b9c2988bf1744f7aa54c7eef277e588749
Tin Nguyen <nguyenvb.tin03@gmail.com>
2026-03-19 02:31:38 -0400

    update: change default breadcrumbs options

--- quartz/components/Breadcrumbs.tsx
+++ quartz/components/Breadcrumbs.tsx
@@ -29,8 +29,8 @@ interface BreadcrumbOptions {
 }
 
 const defaultOptions: BreadcrumbOptions = {
-  spacerSymbol: "❯",
-  rootName: "Home",
+  spacerSymbol: "/",
+  rootName: "🌳",
   resolveFrontmatterTitle: true,
   showCurrentPage: true,
 }
