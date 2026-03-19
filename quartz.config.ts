9174d699593755f5810576baf2ecd24d1f7892c6
Tin Nguyen <nguyenvb.tin03@gmail.com>
2026-03-19 02:12:38 -0400

    update: font to eb garamond

--- quartz.config.ts
+++ quartz.config.ts
@@ -21,9 +21,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "EB Garamond",
+        body: "EB Garamond",
+        code: "Fira Code",
       },
       colors: {
         lightMode: {
